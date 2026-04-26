528384eefe88ebb72dcb60b78dd4064bb9a1e312
Xintian Liu <xintianliu2018@gmail.com>
2026-04-27 01:06:07 +0200

    Finish script comments (7/7)

--- Scripts/animation.py
+++ Scripts/animation.py
@@ -2,19 +2,25 @@ import streamlit as st
 from matplotlib.animation import FuncAnimation
 
 
+# The main function for making animations for the 3 algorithms in app.py
 def make_ani(fig, update_fun, time_range, window, step, filename, fargs):
+    # Sanity check: fargs[0] must be the figure object, as expected by update_fun
     assert fargs[0] is fig, "Please check your input arguments :)"
     ani = FuncAnimation(
         fig,
         update_fun,
+        # Animate from longest to shortest BP, i.e., latest to newest time bin, stepping by "step"
+        # The "+ window - step" ensures that timebin in the last frame ends exactly at the BP selected by user.
         frames=range(time_range[1], time_range[0] + window - step, -step),
         fargs=fargs,
         repeat=False,
     )
+    # Save animation to disk as a video file
     ani.save(filename, fps=8)
 
-    video_file = open(filename, "rb")
-    video_bytes = video_file.read()
+    # Read the saved files and stream it in the Streamlit app
+    with open(filename, "rb") as video_file:
+        video_bytes = video_file.read()
 
     st.video(video_bytes)
     # let the user know where the video is saved
--- Scripts/app.py
+++ Scripts/app.py
@@ -6,19 +6,19 @@ Description:
 This program will run a website, and according to user's choice, generate a single mordern genetic heatmap,
 multiple ancient genetic heatmaps across time bins, and/or an animation of ancient genetic heatmaps
 using algorithms from corresponding imported scripts.
-All the genetic distances are calculated with reference to Icelandic Vikings.
+All the genetic distances are calculated with reference to Icelandic Vikings (CultureID = 1000).
 
 User-defined module: animation, interpolation, linear_migration, modern, multimap, site_turnover
 Non-standard modules: matplotlib, pandas, streamlit
 
 Procedure:
-0. Open internal data and offer options on the web page
+0. Load internal data and offer options on the web page
 1. Execute different logics (modern map, multiple maps, animation) based on the user's choice
-    1.5. If animation is chosen, allow the user customize parameters
+    1.5. If animation is chosen, allow the user to select algorithm and customize parameters
 2. Display the generated plots or video on the web page
 
-Input: None
-Output: A web app, animation videos [optional]
+Input: None (however, internal dataset required)
+Output: A web app, visualizations, animation videos [optional]
 
 Usage : source .venv/bin/activate
         streamlit run Scripts/app.py
@@ -38,60 +38,63 @@ import pandas as pd
 import site_turnover
 import streamlit as st
 
-# Prepare data
+## ===== Prepare data =====
 df_aDNA = pd.read_excel("Dataset/Vikings_aDNA.xlsx")
 df_modern = pd.read_excel("Dataset/ModernSamples/GeneticDistances.xlsx")
 
 st.title("Viking Genetic Heatmap Navigator")
 
+## ===== Mordern dataset (global) =====
 if st.button("Modern Dataset"):
+    # Call function from modern
     fig = modern.plot_heatmap(df_modern)
     st.pyplot(fig)
 
+## ===== Acient dataset (Europe): static multi-map =====
 if st.button("Acient Dataset - Multiple Maps"):
+    # Calculate the genetic distances
     df_aDNA = multimap.aDNA_pre_process(df_aDNA)
     with st.spinner("Calculating Heatmaps..."):
-        # Assign individuals to time bins (use the middle point of each 100 year)
+        # Assign individuals to time bins (use the midpoint of each 100 year)
         df_aDNA["Time_Bin"] = (df_aDNA["Mean date (BP)"] // 100) * 100 + 50
-        # Loop over the time bins
-        # for t in sorted(df_aDNA["Time_Bin"].unique()):
-        # current_bin = df_aDNA[df_aDNA["Time_Bin"] == t]
-        # Call the plotting function
-        # fig = alg.plot_acient_bin(current_bin, int(1950 - t))
-        # st.pyplot(fig)
 
-        # Create as many columns as there are years
+        # Display plots in a 2-column grid
         cols = st.columns(2)
         n_plot = 0
         skipped = []
 
+        # Loop over the time bins
         for t in sorted(df_aDNA["Time_Bin"].unique()):
             current_bin = df_aDNA[df_aDNA["Time_Bin"] == t]
-            # 1. Generate the fig using your logic function
-            yr = int(1950 - t)
+            yr = int(1950 - t)  # Covert BP to calendar year
             fig = multimap.plot_acient_bin(current_bin, yr)
             if fig:
                 with cols[n_plot % 2]:
-                    # 2. Display in the specific column
                     st.subheader(f"Year: {yr}")
                     st.pyplot(fig)
                 n_plot += 1
             else:
+                # Track skipped bins due to not enough data points
                 skipped.append(yr)
-                # st.markdown(f"Skipping {yr}: Too few data points.")
+
         if skipped:
             st.write(f":large_orange_diamond: **Skipped {len(skipped)} time bins due to too few data points:** " + ", ".join(map(str, skipped)))
 
+## ===== Acient dataset (Europe): animation =====
+
+# Use session state to keep the animation UI visible after button click,
+# since Streamlit reruns the script on every interaction
 if "show_animation_ui" not in st.session_state:
     st.session_state.show_animation_ui = False
 
 if st.button("Ancient Dataset - Animation"):
     st.session_state.show_animation_ui = True
 
-# 3. App runs the logic and shows the plot
 if st.session_state.show_animation_ui:
+    # Calculate distances (same as in multimap)
     df_aDNA = multimap.aDNA_pre_process(df_aDNA)
-    # 1. User selects algorithm in sidebar
+
+    # Let the user choose algorithm
     algo = st.selectbox(
         "Please Choose Algorithm",
         ("3D-RBF", "Site Turnover", "Linear Migration"),
@@ -99,9 +102,10 @@ if st.session_state.show_animation_ui:
         placeholder="Select to continue",
     )
 
-    # 2. User selects timeframe
+    # Specify time range and sliding window parameters
     time_range = st.slider("BP Range", 700, 1300, (750, 1250), step=5)
     st.caption(":red-background[NOTE:] *BP = the number of years before 1950*")
+
     window = st.number_input(
         "Specify window size (defualt 100)",
         min_value=10,
@@ -110,10 +114,14 @@ if st.session_state.show_animation_ui:
     )
     step = st.number_input("Specify step size (defualt 10)", min_value=1, max_value=100, value=10)
 
+    # ===== Algo 1: 3D Rbf =====
     if algo == "3D-RBF" and st.button("Generate Animation"):
         with st.spinner("Calculating Heatmaps..."):
+            # Fit a 3D rbf model across (Lat, Long, Time) using the full dataset
             rbf_3d = interpolation.create_3drbf(df_aDNA)
             fig = plt.figure(figsize=(10, 7))
+
+            # Make animation: be careful about input arguments
             animation.make_ani(
                 fig,
                 interpolation.update,
@@ -124,10 +132,13 @@ if st.session_state.show_animation_ui:
                 (fig, df_aDNA, rbf_3d, window),
             )
 
+    # ===== Algo 2: Site Turnover =====
     if algo == "Site Turnover" and st.button("Generate Animation"):
         with st.spinner("Calculating Heatmaps..."):
+            # Create a uniq siteID per geographic location
             df_aDNA["Site_ID"] = df_aDNA["Lat"].astype(str) + "_" + df_aDNA["Long"].astype(str)
             fig = plt.figure(figsize=(10, 7))
+            # Track the latest known gen-distance per location across time/frames
             latest_world_state = {}
 
             animation.make_ani(
@@ -140,10 +151,13 @@ if st.session_state.show_animation_ui:
                 (fig, df_aDNA, window, latest_world_state),
             )
 
+    # ===== Algo 3: Linear Migration =====
     if algo == "Linear Migration" and st.button("Generate Animation"):
         with st.spinner("Calculating Heatmaps..."):
             fig = plt.figure(figsize=(10, 7))
+            # Track the latest known gen-distance info per cultural group across time/frames
             latest_world_state = {}
+
             animation.make_ani(
                 fig,
                 linear_migration.update,
